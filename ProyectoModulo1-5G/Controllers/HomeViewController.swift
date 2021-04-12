a113a8b37b2270a1433d8025fd6eb2f9b197bab3
Berenice Medel <bemesa21@gmail.com>
2021-04-11 22:37:27 -0500

    add a tab controller and photocollectionViewController, images downloaded from firebase

--- ProyectoModulo1-5G/Controllers/HomeViewController.swift
+++ ProyectoModulo1-5G/Controllers/HomeViewController.swift
@@ -7,23 +7,21 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+        guard let PhotosControllerTemp = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "PhotoCollectionViewController") as? PhotoCollectionViewController else{ return }
+        let PhotosController = UINavigationController(rootViewController: PhotosControllerTemp)
+        
+        PhotosController.tabBarItem.title = "Fotos"
+        PhotosController.tabBarItem.image = UIImage(systemName: "doc.richtext")
+        
+        
+        viewControllers = [
+        PhotosController
+        ]
+            }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
